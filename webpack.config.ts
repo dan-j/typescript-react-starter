455385e615e1b2f104e7934fbf725d44ce59e9b6
dan-j <jonesd9@me.com>
2017-07-17 16:19:00 +1000

    Added CSSModules dependency and configured webpack for loading css and improved HMR capability

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,14 @@
-import webpack from 'webpack';
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+// we normally aren't allowed `import` being here because our tsconfig.json file targets ES6
+// modules and `ts-node` doesn't support ES6 (hence the use of webpack). However, because the
+// only place where `Webpack` is referenced is inside type declarations, the import statement is
+// removed at transpilation-time so no error occurs.
+import * as Webpack from 'webpack';
 
-const config: webpack.Configuration = {
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const config: Webpack.Configuration = {
     entry: [
         'react-hot-loader/patch',
         './src/index.tsx',
@@ -29,10 +35,33 @@ const config: webpack.Configuration = {
                 'babel-loader',
                 'awesome-typescript-loader',
             ],
+        }, {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'style-loader',
+                options: {
+                    sourceMap: true,
+                },
+            }, {
+                loader: 'css-loader',
+                options: {
+                    modules: true,
+                    importLoaders: 1,
+                    localIdentName: '[name]__[local]___[hash:base64:5]',
+                },
+            }, {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true,
+                },
+            }],
         }],
     },
 
     plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
         new CopyWebpackPlugin([{
             from: 'src/index.html',
         }]),
@@ -41,8 +70,7 @@ const config: webpack.Configuration = {
     devServer: {
         port: 8080,
         historyApiFallback: true,
-        inline: true,
-        hot: true,
+        hotOnly: true,
         contentBase: path.resolve(__dirname, 'dist/'),
     },
 };
